b44b6e65d755906eb7af88154a120334b97381d1
kashefi <kashefy@rayakavosh.ir>
2021-02-12 22:15:30 +0330

    add laravel jwt

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        \Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
         UxWeb\SweetAlert\SweetAlertServiceProvider::class,
         /*
          * Application Service Providers...
@@ -227,7 +228,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Alert' => UxWeb\SweetAlert\SweetAlert::class,
-
+        'JWTAuth' => \Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => \Tymon\JWTAuth\Facades\JWTFactory::class,
     ],
 
 ];
